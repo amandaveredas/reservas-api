0837597b814097b44ed4b91d6dec5db8ed0c2500
amandaveredas <amandaveredas@gmail.com>
2021-11-09 15:30:11 -0300

    FIX: Recebe periodo como parâmetro nos métodos de listagem da ReservaController
    Antes estava recebendo dois parâmetros de data, o que feria o contrato
    Insere anotação @DateTimeFormat direto na Periodo, retirando da controller, pois não funcionava.

--- src/main/java/io/github/cwireset/tcc/controller/ReservaController.java
+++ src/main/java/io/github/cwireset/tcc/controller/ReservaController.java
@@ -1,8 +1,6 @@
 package io.github.cwireset.tcc.controller;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
 import io.github.cwireset.tcc.domain.FormaPagamento;
-import io.github.cwireset.tcc.domain.Imovel;
 import io.github.cwireset.tcc.domain.Periodo;
 import io.github.cwireset.tcc.domain.Reserva;
 import io.github.cwireset.tcc.exception.*;
@@ -14,14 +12,10 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
-import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import springfox.documentation.annotations.ApiIgnore;
 
-
-import javax.persistence.EnumType;
-import javax.persistence.Enumerated;
 import javax.validation.Valid;
 import java.time.LocalDateTime;
 
@@ -40,16 +34,16 @@ public class ReservaController {
 
     @GetMapping("/solicitantes/{idSolicitante}")
     public Page<Reserva> listarPorSolicitante( @PathVariable Long idSolicitante,
-                                               @RequestParam (required = false) @DateTimeFormat(pattern="yyyy-MM-dd HH:mm:ss") LocalDateTime dataHoraInicial,
-                                               @RequestParam (required = false) @DateTimeFormat(pattern="yyyy-MM-dd HH:mm:ss") LocalDateTime dataHoraFinal,
+                                              Periodo periodo,
                                                @PageableDefault(sort = "periodo_dataHoraFinal",
-            direction = Sort.Direction.DESC,
-            page = 0,
-            size = 10) @ApiIgnore Pageable pageable ){
+            direction = Sort.Direction.DESC) @ApiIgnore Pageable pageable ){
 
+        LocalDateTime dataHoraInicial = periodo.getDataHoraInicial();
+        LocalDateTime dataHoraFinal = periodo.getDataHoraFinal();
 
         return reservaService.buscarReservasPorSolicitante(idSolicitante, dataHoraInicial,dataHoraFinal, pageable);
     }
+
 
     @GetMapping("/anuncios/anunciantes/{idAnunciante}")
     public Page<Reserva> listarPorAnunciante( @PathVariable Long idAnunciante,
--- src/main/java/io/github/cwireset/tcc/domain/Periodo.java
+++ src/main/java/io/github/cwireset/tcc/domain/Periodo.java
@@ -14,8 +14,10 @@ import java.time.LocalDateTime;
 public class Periodo {
 
     @JsonFormat(pattern="yyyy-MM-dd HH:mm:ss")
+    @DateTimeFormat(pattern="yyyy-MM-dd HH:mm:ss")
     private LocalDateTime dataHoraInicial;
     @JsonFormat(pattern="yyyy-MM-dd HH:mm:ss")
+    @DateTimeFormat(pattern="yyyy-MM-dd HH:mm:ss")
     private LocalDateTime dataHoraFinal;
 
 }
